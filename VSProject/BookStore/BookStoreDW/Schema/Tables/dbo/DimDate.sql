20b37c3b208677b637ff8ceed9f4d8f663a5b18d
j <juliocadima29@gmail.com>
2025-05-03 14:24:46 -0400

    tablas creadas: DimDate y PackageConfig

---
+++ VSProject/BookStore/BookStoreDW/Schema/Tables/dbo/DimDate.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[DimDate]
+(
+	[dateKey] [int] NOT NULL CONSTRAINT PK_DimDate PRIMARY KEY,
+	[fullDate] [date] NOT NULL,
+	[dayNumberOfWeek] [tinyint] NOT NULL,
+	[dayNameOfWeek] [nvarchar](10) NOT NULL,
+	[dayNumberOfMonth] [tinyint] NOT NULL,
+	[dayNumberOfYear] [smallint] NOT NULL,
+	[weekNumberOfYear] [tinyint] NOT NULL,
+	[monthName] [nvarchar](10) NOT NULL,
+	[monthNumberOfYear] [tinyint] NOT NULL,
+	[calendarQuarter] [tinyint] NOT NULL,
+	[calendarYear] [smallint] NOT NULL,
+	[calendarSemester] [tinyint] NOT NULL
+);
+GO
---
+++ VSProject/BookStore/BookStoreDW/Schema/Tables/dbo/PackageConfig.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[PackageConfig]
+(
+	[packageId] [int] IDENTITY(1,1) NOT NULL CONSTRAINT pk_PackageConfig PRIMARY KEY,
+	[tableName] [varchar](50) NOT NULL,
+	[lastRowVersion] [bigint] NULL
+)
+GO
