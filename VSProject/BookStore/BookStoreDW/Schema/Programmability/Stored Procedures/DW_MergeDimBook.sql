15123934ab2f5578bf73ac9d481bc3b39dd4006b
Edwin <egonzalesp@gmail.com>
2025-05-03 13:53:20 -0400

    MergeDimBook

---
+++ VSProject/BookStore/BookStoreDW/Schema/Programmability/Stored Procedures/DW_MergeDimBook.sql
@@ -0,0 +1,24 @@
+﻿USE [BookStoreDW]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[DW_MergeDimBook]
+AS
+BEGIN
+
+	UPDATE db
+	SET 
+         [title]			= sb.[title]
+        ,[isbn13]			= sb.[isbn13]
+        ,[numPages]			= sb.[numPages]
+        ,[publicationDate]	= sb.[publicationDate]
+        ,[languageName]		= sb.[languageName]
+	FROM [dbo].[DimBook] db
+	INNER JOIN [staging].[Book] sb ON (db.[bookSk]=sb.[bookSk])
+END
+GO
