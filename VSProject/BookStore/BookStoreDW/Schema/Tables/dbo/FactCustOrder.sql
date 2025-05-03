7bc6324f7a8aa90ef35909314a6a6a4c47a4d05a
j <juliocadima29@gmail.com>
2025-05-03 11:39:44 -0400

    tabla de hechos creada del modelo estrella, al igual que su tabla de staging

---
+++ VSProject/BookStore/BookStoreDW/Schema/Tables/dbo/FactCustOrder.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[FactCustOrder]
+(
+	[orderId] INT NOT NULL,
+	[lineId] INT NOT NULL,
+	[historyId] INT NOT NULL,
+	[statusId] INT NOT NULL,
+	[orderDateKey] INT NOT NULL,
+	[statusDateKey] INT NOT NULL,
+	[bookSk] INT NOT NULL,
+	[customerSk] INT NOT NULL,
+	[destDeliverySiteSk] INT NOT NULL,
+	[shippingMethodSk] INT,
+	[price] DECIMAL(5, 2),
+	[orderDate] DATETIME,
+	[statusDate] DATETIME,
+	[statusValue] VARCHAR(20),
+	CONSTRAINT pk_FactCustOrder PRIMARY KEY([orderId],[lineId],[historyId], [statusId]),
+	CONSTRAINT fk_DimBook FOREIGN KEY([bookSk]) REFERENCES [dbo].[DimBook]([bookSk]),
+	CONSTRAINT fk_DimCustomer FOREIGN KEY([customerSk]) REFERENCES [dbo].[DimCustomer]([customerSk]),
+	CONSTRAINT fk_DimDeliverySite FOREIGN KEY([destDeliverySiteSk]) REFERENCES [dbo].[DimDeliverySite]([deliverySiteSk]),
+	CONSTRAINT fk_DimShippingMethod FOREIGN KEY([shippingMethodSk]) REFERENCES [dbo].[DimShippingMethod]([shippingMethodSk])
+)
+GO
---
+++ VSProject/BookStore/BookStoreDW/Schema/Tables/staging/staging.custOrder.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [staging].[order]
+(
+	[orderId] INT NOT NULL,
+	[lineId] INT NOT NULL,
+	[historyId] INT NOT NULL,
+	[statusId] INT NOT NULL,
+	[orderDateKey] INT NOT NULL,
+	[statusDateKey] INT NOT NULL,
+	[bookSk] INT NOT NULL,
+	[customerSk] INT NOT NULL,
+	[destDeliverySiteSk] INT NOT NULL,	
+	[shippingMethodSK] INT,
+	[price] DECIMAL(5, 2),
+	[orderDate] DATETIME,
+	[statusDate] DATETIME,
+	[statusValue] VARCHAR(20)
+)
+GO
