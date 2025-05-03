962cd73a3d9e41867300f6de0ade4caeb5f69ac8
j <juliocadima29@gmail.com>
2025-05-03 11:42:12 -0400

    Tabla de dimension creada DimDeliverySite, esta tabla tiene los datos de las tablas del OLTP customer_address, address_status, address, y country. Tambien se creo la tabla de staging.

---
+++ VSProject/BookStore/BookStoreDW/Schema/Tables/dbo/DimDeliverySite.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[DimDeliverySite]
+(
+	[deliverySiteSk] INT IDENTITY(1,1) NOT NULL,
+    [addressId] INT NOT NULL,
+	[streetNumber] VARCHAR(10),
+    [streetName] VARCHAR(200),
+    [city] VARCHAR(100),
+    [countryName] VARCHAR(200),
+    [addressStatus] VARCHAR(30),
+	CONSTRAINT pk_Address PRIMARY KEY ([deliverySiteSk])
+)
+GO
---
+++ VSProject/BookStore/BookStoreDW/Schema/Tables/staging/staging.deliverySite.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [staging].[deliverySite]
+(
+	[deliverySiteSk] INT NOT NULL,
+	[streetNumber] VARCHAR(10),
+    [streetName] VARCHAR(200),
+    [city] VARCHAR(100),
+    [countryName] VARCHAR(200),
+    [addressStatus] VARCHAR(30)
+)
+GO
