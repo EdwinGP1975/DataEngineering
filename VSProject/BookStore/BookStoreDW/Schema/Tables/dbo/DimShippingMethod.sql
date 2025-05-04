00a6f8b1126773e6c92632fa0193b1f6cb1cdee6
Jose Oña <fernandocarrasc590@gmail.com>
2025-05-04 11:08:37 -0400

    Agregando DimAuthor y DimShippingMethod

---
+++ VSProject/BookStore/BookStoreDW/Schema/Tables/dbo/DimShippingMethod.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[DimShippingMethod]
+(
+	[shippingMethodSk] [int] IDENTITY(1,1) NOT NULL,
+	[shippingMethodId] [int] NOT NULL,
+	[methodName] [varchar](100) NULL,
+	[cost] [decimal](6, 2) NULL,
+	CONSTRAINT [pk_ShippingMethod] PRIMARY KEY ([shippingMethodSk])
+)
