022fb29cd20a795938756a9f8feeae342197a7be
Ariel <ariel@toogonet.com>
2025-05-04 18:02:15 -0400

    create Dim customer and stage customer

---
+++ VSProject/BookStore/BookStoreDW/Schema/Tables/dbo/DimCustomer.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[DimCustomer]
+(
+	[customerSk] INT NOT NULL IDENTITY(1,1),
+	[customerId] INT NOT NULL,
+	[firstName] VARCHAR(200) NULL, 
+	[lastName] VARCHAR(200) NULL, 
+	[email] VARCHAR(350) NULL,
+	CONSTRAINT pk_DimCustomer PRIMARY KEY ([customerSk])
+)
---
+++ VSProject/BookStore/BookStoreDW/Schema/Tables/staging/staging.customer.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [staging].[Customer]
+(
+	[customerSk] [int] IDENTITY(1,1) NOT NULL,
+	[firstName] VARCHAR(200) NULL, 
+	[lastName] VARCHAR(200) NULL, 
+	[email] VARCHAR(350) NULL,
+)
