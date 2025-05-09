679bba73f5fe7dfb895fba8ce4b50843128edd1d
Ariel <ariel@toogonet.com>
2025-05-09 00:08:57 -0400

    ETL customer

---
+++ VSProject/BookStore/BookStoreOLTP/Schema/Programmability/GetCustomerChangesByRowVersion.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[GetCustomerChangesByRowVersion]
+(
+   @startRow BIGINT 
+   ,@endRow  BIGINT 
+)
+AS
+BEGIN
+ SELECT bk.[customer_id]
+        ,bk.[first_name]
+        ,bk.[last_name]
+        ,bk.[email]    
+   FROM [BookStore].[dbo].[customer] bk
+   WHERE (bk.[rowversion] > CONVERT(ROWVERSION,@startRow) 
+          AND bk.[rowversion] <= CONVERT(ROWVERSION,@endRow))
+END
+GO
