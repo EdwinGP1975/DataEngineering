730db70f7b052452c91792766b3ef2bd6f64c598
Victor Estares <victor.estaresss@dualbiz.net>
2025-05-10 02:49:33 -0400

    ADD: ETL PUBLISHER

---
+++ VSProject/BookStore/BookStoreOLTP/Schema/Programmability/GetPublisherChangesByRowVersion.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[GetPublisherChangesByRowVersion]
+(
+   @startRow BIGINT 
+   ,@endRow  BIGINT 
+)
+AS
+BEGIN
+ SELECT pb.[publisher_id],
+        pb.[publisher_name]
+        ,bk.[title]
+        ,bk.[publication_date]
+        ,pb.[rowversion]
+   FROM [dbo].[publisher] pb
+   inner join [dbo].[book] bk on pb.publisher_id=bk.publisher_id
+   WHERE (pb.[rowversion] > CONVERT(ROWVERSION,@startRow) 
+          AND pb.[rowversion] <= CONVERT(ROWVERSION,@endRow))
+    OR
+	     (bk.[rowversion] > CONVERT(ROWVERSION,@startRow) 
+          AND bk.[rowversion] <= CONVERT(ROWVERSION,@endRow));
+END
+GO
